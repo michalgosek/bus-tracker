b67e7e85d9bcc5637a17dbfba6fdec8a74efef67
Michał Gosek <michalgosek@MacBook-Pro-Micha.local>
2020-05-10 17:57:51 +0200

    Initial commit.

---
+++ src/main/java/com/example/bustracker/BusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTrackerApplication.class, args);
+	}
+
+}
