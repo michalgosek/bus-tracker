26689fa49446066d41502f0aa143682a3a662594
Michal Gosek <inbox@michalgosek.com>
2020-05-19 21:32:52 +0200

    Added UserDetailsService implementation.

--- src/main/java/com/example/bustracker/authentication/ApplicationUserService.java
+++ src/main/java/com/example/bustracker/authentication/ApplicationUserService.java
@@ -1,4 +1,26 @@
 package com.example.bustracker.authentication;
 
-public class ApplicationUserService {
+import com.example.bustracker.persistence.dao.ApplicationUserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationUserService implements UserDetailsService {
+
+    @Autowired
+    public ApplicationUserService(@Qualifier("MySQL") ApplicationUserDAO applicationUserDAO) {
+        this.applicationUserDAO = applicationUserDAO;
+    }
+
+    private final ApplicationUserDAO applicationUserDAO;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return applicationUserDAO.loadUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("% not found.%n", username)));
+    }
 }
