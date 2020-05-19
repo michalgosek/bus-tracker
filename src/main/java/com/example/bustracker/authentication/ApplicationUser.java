fb2472fa7d6dd7550ed6224713d434a7f7cad24f
Michal Gosek <inbox@michalgosek.com>
2020-05-19 21:31:55 +0200

    Added UserDetails implementation.

--- src/main/java/com/example/bustracker/authentication/ApplicationUser.java
+++ src/main/java/com/example/bustracker/authentication/ApplicationUser.java
@@ -1,4 +1,54 @@
 package com.example.bustracker.authentication;
 
-public class ApplicationUser {
+
+import org.springframework.security.core.GrantedAuthority;
+import com.example.bustracker.persistence.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class ApplicationUser implements UserDetails {
+
+    private final User user;
+    private final Collection<? extends GrantedAuthority> grantedAuthorities;
+
+    public ApplicationUser(User user, Collection<? extends GrantedAuthority> grantedAuthorities) {
+        this.user = user;
+        this.grantedAuthorities = grantedAuthorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return user.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return user.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
 }
