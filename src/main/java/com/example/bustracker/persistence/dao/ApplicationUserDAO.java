bde7040c3098774f27dcbea2a62c0479b184cf59
Michal Gosek <inbox@michalgosek.com>
2020-05-19 21:40:01 +0200

    Added ApplicationUser DAO.

--- src/main/java/com/example/bustracker/persistence/dao/ApplicationUserDAO.java
+++ src/main/java/com/example/bustracker/persistence/dao/ApplicationUserDAO.java
@@ -1,4 +1,9 @@
 package com.example.bustracker.persistence.dao;
 
+import com.example.bustracker.authentication.ApplicationUser;
+
+import java.util.Optional;
+
 public interface ApplicationUserDAO {
+    Optional<ApplicationUser> loadUserByUsername(String username);
 }
