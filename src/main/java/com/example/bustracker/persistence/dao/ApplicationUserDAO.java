ef919ffe628c2b125167c4b30a22931c94502486
Michal Gosek <inbox@michalgosek.com>
2020-06-11 14:02:37 +0200

    Added UsernameNotFoundException to ApplicationUserDAO.

--- src/main/java/com/example/bustracker/persistence/dao/ApplicationUserDAO.java
+++ src/main/java/com/example/bustracker/persistence/dao/ApplicationUserDAO.java
@@ -1,8 +1,10 @@
 package com.example.bustracker.persistence.dao;
 
 import com.example.bustracker.authentication.ApplicationUser;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import java.util.Optional;
 
-public interface ApplicationUserDAO {
-    Optional<ApplicationUser> loadUserByUsername(String username);
+public interface ApplicationUserDAO{
+    Optional<ApplicationUser> loadUserByUsername(String username) throws UsernameNotFoundException;
 }
