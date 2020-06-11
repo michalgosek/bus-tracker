187aa71c70f2c899204e1c01177795b7b1ecea4e
Michal Gosek <inbox@michalgosek.com>
2020-06-11 21:19:30 +0200

    Fixed logging error if user not exist.

--- src/main/java/com/example/bustracker/authentication/MySQLDataAccessService.java
+++ src/main/java/com/example/bustracker/authentication/MySQLDataAccessService.java
@@ -3,9 +3,9 @@ package com.example.bustracker.authentication;
 import com.example.bustracker.persistence.dao.ApplicationUserDAO;
 import com.example.bustracker.persistence.model.Role;
 import com.example.bustracker.persistence.model.User;
-import com.example.bustracker.security.ApplicationUserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -24,7 +24,7 @@ public class MySQLDataAccessService implements ApplicationUserDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    private List<User> getApplicationUsers() {
+    private Optional<User> selectUser(String userName) {
         final String query = "SELECT id, " +
                 "username, " +
                 "password, " +
@@ -32,27 +32,19 @@ public class MySQLDataAccessService implements ApplicationUserDAO {
                 "is_account_non_locked, " +
                 "is_credentials_non_expired, " +
                 "is_enabled\n" +
-                "FROM users;";
-
-        return jdbcTemplate.query(query, mapUserFromDatabase());
+                "FROM users\n" +
+                "WHERE username = ?";
+        return Optional.ofNullable(jdbcTemplate.query(query, new Object[]{userName}, mapUserFromDatabase()));
     }
 
-    private List<Role> getApplicationRoles() {
+    private List<Long> selectUserRoles(Long id) {
+        final String query = "SELECT role_id FROM users_roles WHERE user_id = ?";
+        return jdbcTemplate.query(query, new Object[]{id}, mapUserRolesFromDatabase());
+    }
+
+    private List<Role> selectAllRoles() {
         final String query = "SELECT id, name FROM roles";
         return jdbcTemplate.query(query, mapRolesFromDatabase());
-    }
-
-    private List<ApplicationUserRole> getApplicationUserRole() {
-        final String query = "SELECT user_id, role_id FROM users_roles";
-        return jdbcTemplate.query(query, mapApplicationUserRole());
-    }
-
-    private RowMapper<ApplicationUserRole> mapApplicationUserRole() {
-        return (resultSet, i) -> {
-            final Long userId = resultSet.getLong("user_id");
-            final Long roleId = resultSet.getLong("role_id");
-            return new ApplicationUserRole(userId, roleId);
-        };
     }
 
     private RowMapper<Role> mapRolesFromDatabase() {
@@ -63,42 +55,46 @@ public class MySQLDataAccessService implements ApplicationUserDAO {
         };
     }
 
-    private RowMapper<User> mapUserFromDatabase() {
-        return (resultSet, i) -> {
-            final Long id = resultSet.getLong("id");
-            final String userName = resultSet.getString("username");
-            final String password = resultSet.getString("password");
-            final boolean isAccountNotExpired = resultSet.getBoolean("is_account_non_expired");
-            final boolean isAccountNotLocked = resultSet.getBoolean("is_account_non_locked");
-            final boolean isCredentialsNonExpired = resultSet.getBoolean("is_credentials_non_expired");
-            final boolean isEnabled = resultSet.getBoolean("is_enabled");
+    private RowMapper<Long> mapUserRolesFromDatabase() {
+        return (rs, rowNum) -> rs.getLong("role_id");
+    }
 
-            return new User(
-                    id,
-                    userName,
-                    password,
-                    isAccountNotExpired,
-                    isAccountNotLocked,
-                    isCredentialsNonExpired,
-                    isEnabled
-            );
+    private ResultSetExtractor<User> mapUserFromDatabase() {
+        return resultSet -> {
+            if (resultSet.next()) {
+                final Long id = resultSet.getLong("id");
+                final String userName = resultSet.getString("username");
+                final String password = resultSet.getString("password");
+                final boolean isAccountNotExpired = resultSet.getBoolean("is_account_non_expired");
+                final boolean isAccountNotLocked = resultSet.getBoolean("is_account_non_locked");
+                final boolean isCredentialsNonExpired = resultSet.getBoolean("is_credentials_non_expired");
+                final boolean isEnabled = resultSet.getBoolean("is_enabled");
+
+                return new User(
+                        id,
+                        userName,
+                        password,
+                        isAccountNotExpired,
+                        isAccountNotLocked,
+                        isCredentialsNonExpired,
+                        isEnabled);
+            } else {
+                return null;
+            }
         };
     }
 
     @Override
     public Optional<ApplicationUser> loadUserByUsername(String username) {
-        final User appUser = getApplicationUsers().stream()
-                .filter(user -> username.equals(user.getUsername())).findFirst()
-                .orElseThrow(() -> new UsernameNotFoundException(username));
 
-        final Map<Long, String> roles = getApplicationRoles().stream()
+        final User appUser = selectUser(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        final Map<Long, String> roles = selectAllRoles().stream()
                 .collect(Collectors.toMap(Role::getId, Role::getName));
 
-        final Set<SimpleGrantedAuthority> authorities =
-                getApplicationUserRole().stream()
-                        .filter(user -> user.getUserId().equals(appUser.getId()))
-                        .map(user -> new SimpleGrantedAuthority(roles.get(user.getRoleId())))
-                        .collect(Collectors.toSet());
+        final Set<SimpleGrantedAuthority> authorities = selectUserRoles(appUser.getId())
+                .stream().map(roleID -> new SimpleGrantedAuthority(roles.get(roleID)))
+                .collect(Collectors.toSet());
 
         return Optional.of(new ApplicationUser(appUser, authorities));
     }
